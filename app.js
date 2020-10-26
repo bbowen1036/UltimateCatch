b42e78f386595d99f2af41e4738613a1e390a6af
Zach Vaillancourt <zsvail98@live.com>
2020-10-26 10:12:16 -0700

    add db var

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 const users = require("./routes/api/users");
 const bodyParser = require('body-parser');
+const db = require('./config/keys').mongoURI;
 
 app.use("/api/users", users);
 app.use(bodyParser.urlencoded({ extended: false }));
