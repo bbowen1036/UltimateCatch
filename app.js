bd12390a712b764d363376e29e147f1f56333efe
brianbowen <bbowen1036@gmail.com>
2020-10-26 14:22:35 -0700

    backend user auth

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const port = process.env.PORT || 5000;
-const users = require("./routes/api/users");
-const bodyParser = require('body-parser');
 const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const passport = require('passport');
+const bodyParser = require('body-parser');
 
-app.use("/api/users", users);
+
+const users = require("./routes/api/users");
+
+mongoose
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello Wdddorld"));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+app.use("/api/users", users);
 
-app.get("/", (req, res) => res.send("Hello World"));
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
