5df6aabd9ef63d3da7b98f776dd0df4c9506eab4
Zach Vaillancourt <zsvail98@live.com>
2020-10-28 16:57:59 -0700

    state update for like

--- frontend/src/components/posts/post_index_item.js
+++ frontend/src/components/posts/post_index_item.js
@@ -4,7 +4,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class PostIndexItem extends React.Component{
     constructor(props){
-        super(props);
+        this.state = {
+            likes: this.props.likes
+        }
+        this.handleLike = this.handleLike.bind(this);
+    }
+
+    handleLike(){
+        return e => {
+            this.setState({likes: this.state.likes.push(this.props.heartPost(this.props.post))})
+        }
     }
 
     render(){
@@ -13,8 +22,8 @@ class PostIndexItem extends React.Component{
                 <div className="post-pic-container"></div>
                 <div className="post-info">
                     <h3>{this.props.post.text}</h3>
-                    <h3>{this.props.post.likes.length} likes</h3>
-                    <FontAwesomeIcon icon={faHeart} onClick={() => this.props.heartPost(this.props.post)}/>
+                    <h3>{this.state.likes.length} likes</h3>
+                    <FontAwesomeIcon icon={faHeart} onClick={() => this.handleLike()}/>
                     <FontAwesomeIcon icon={faComment}/>
                 </div>
             </div>
