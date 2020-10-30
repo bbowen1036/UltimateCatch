8c5966d1c40fe7efa17910d3cdf22cd33db1a052
Zach Vaillancourt <zsvail98@live.com>
2020-10-30 13:54:02 -0700

    stuff

--- frontend/src/components/map/map.js
+++ frontend/src/components/map/map.js
@@ -136,14 +136,6 @@ function Map(props){
                     }}
                     >
                         <div>
-<<<<<<< HEAD
-                            <h2>
-                                We will put post modal here?
-                                <p>{selected.lat} {selected.lng}</p>
-                                <p onClick={() => props.handleRegionChange(selected.region_id)}> Go to Posts </p>
-                            </h2>
-                            <p>{formatRelative(selected.time, new Date())}</p>
-=======
                             {/* <h2> */}
                                 {/* We will put post modal here? */}
                                 {/* <p>{selected.lat} {selected.lng}</p>
@@ -163,7 +155,6 @@ function Map(props){
                                 {/* <Post fetchpost={props.fetchPost} lat={selected.lat} lng={selected.lng}/> */}
                             {/* </h2> */}
                             {/* <p>{formatRelative(selected.time, new Date())}</p> */}
->>>>>>> bda23a5011193b6fd757a425c870f1f53a7e80fd
                         </div>
                     </InfoWindow>
                 ) : null}
